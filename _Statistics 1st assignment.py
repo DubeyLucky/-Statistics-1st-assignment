00539c59697d6576c465abdc61bf2f8ea60f177f
DubeyLucky <114846353+DubeyLucky@users.noreply.github.com>
2023-09-24 20:38:56 +0530

    Create _Statistics 1st assignment.py

---
+++ _Statistics 1st assignment.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is Statistics?
+
+#  Statistics is the science of collecting, organising and analyzing data.
+#     
+
+# Q2. Define the different types of statistics and give an example of when each type might be used.
+There are two types of statisics 
+1.Descriptive 
+2.Inferential 
+1.Descriptive :-
+    Measure of frequency
+    Measure of dispersion(variance, and standard deviation)
+    Measure of central tendency(Mean, Median , Mode)
+    Measure of position
+2.Inferential :-
+    ANOVA
+    chi-square etc
+# Q3. What are the different types of data and how do they differ from each other? Provide an example of
+# each type of data.
+There are 2 types
+Quantitative data and Qualitative data
+Quantitative data :-Data is depicted in numerical terms. Can be shown in numbers and variables like ratio, percentage, and more.Example: 100%, 1:3, 123
+Qualitative data:-Data is not depicted in numerical terms. Could be about the behavioral attributes of a person, or thing. Example: loud behavior, fair skin, soft quality, and more.   Q4. Categorise the following datasets with respect to quantitative and qualitative data types:
+(i) Grading in exam: A+, A, B+, B, C+, C, D, E
+(ii) Colour of mangoes: yellow, green, orange, red
+(iii) Height data of a class: [178.9, 179, 179.5, 176, 177.2, 178.3, 175.8,...]
+(iv) Number of mangoes exported by a farm: [500, 600, 478, 672, ...]quantitative :-(iii) Height data of a class: [178.9, 179, 179.5, 176, 177.2, 178.3, 175.8,...]
+        (iv) Number of mangoes exported by a farm: [500, 600, 478, 672, ...]
+qualitative :-(i) Grading in exam: A+, A, B+, B, C+, C, D, E
+(ii) Colour of mangoes: yellow, green, orange, redQ7. How nominal data type is different from ordinal data type.Nominal:-Nominal data is defined as the data used for naming or labeling variables, without any quantitative value.
+    Used to collect data of people, places, or things.
+    There is no quantitative value associated with variables. It is a qualitative measure.
+    Equality
+    example :-Sex (male and female) 
+ordinal:-Similar to nominal data, ordinal data is categorical data with an order.
+    Used to collect feedback, reviews, or ratings.
+    Quantitative values are attached to ordinal variables, but you can’t conduct arithmetic calculations.
+    ex:-Customer Satisfaction Rating (on a scale 0 – 5)Q8. Which type of plot can be used to display data in terms of range?Line Graph
+Bar Graph
+Pie Chart
+Histogram
+Dot Graph
+Scatter plot
+Bubble chart
+
+# Q9. Describe the difference between descriptive and inferential statistics. Give an example of each
+# type of statistics and explain how they are used.
+
+# In[ ]:
+
+
+There are two types of statisics 
+1.Descriptive 
+2.Inferential 
+1.Descriptive :-
+    Measure of frequency
+    Measure of dispersion(variance, and standard deviation)
+    Measure of central tendency(Mean, Median , Mode)
+    Measure of position
+2.Inferential :-
+    ANOVA
+    chi-square etc
+    
+1.Descriptive :-It gives information about raw data which describes the data in some manner.
+    It helps in organizing, analyzing, and to present data in a meaningful manner.
+    It is used to describe a situation.
+    It explains already known data and is limited to a sample or population having a small size.
+    It can be achieved with the help of charts, graphs, tables, etc.
+    
+2.Inferential :-It makes inferences about the population using data drawn from the population.
+    It allows us to compare data, and make hypotheses and predictions.
+    It is used to explain the chance of occurrence of an event.
+    It attempts to reach the conclusion about the population.
+    It can be achieved by probability.
+
